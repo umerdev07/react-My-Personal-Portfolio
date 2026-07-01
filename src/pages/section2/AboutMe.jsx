d6a40aa65a8dc1a970d26dfab8d4e6e4be012512
Muhammad Umer Yousaf <umeryousaf2020@gmail.com>
2026-07-01 23:24:16 +0500

    Build Skills Section

--- src/pages/section2/AboutMe.jsx
+++ src/pages/section2/AboutMe.jsx
@@ -5,8 +5,8 @@ import RightContent from "./RightContent";
 
 const AboutMe = () => {
   return (
-<div className="max-w-7xl mx-auto px-3 sm:px-5 lg:px-8">
-        <AboutMeTitleSubTitleBadge />
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-[70px] sm:mt-24 lg:mt-28">
+      <AboutMeTitleSubTitleBadge />
 
       <div className="mt-10 lg:mt-20 flex flex-col lg:flex-row gap-8 lg:gap-12">
         <LeftContent />
